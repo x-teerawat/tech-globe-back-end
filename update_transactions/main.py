294b0f7f2b477e62b8771963d49ca7ef0df9a1f3
Teerawat Xu <xu.teerawat@gmail.com>
2024-07-15 21:36:14 +0700

    update transactions to tg back end database

--- update_transactions/main.py
+++ update_transactions/main.py
@@ -75,15 +75,16 @@ class UpdateTransactions():
         n_transactions_in_tg_back_end = self.transactions_of_tg_back_end.count_documents({})
         
         if n_transactions_in_tg > n_transactions_in_tg_back_end:
-            IsUpdate = True
+            self.IsUpdate = True
         elif n_transactions_in_tg == n_transactions_in_tg_back_end:
-            IsUpdate = False
+            self.IsUpdate = False
         else:
             print(f"n_transactions_in_tg ({n_transactions_in_tg}) < n_transactions_in_tg_back_end ({n_transactions_in_tg_back_end})")
+            self.IsUpdate = None
         
         print(f"n_transactions_in_tg: {n_transactions_in_tg}")
         print(f"n_transactions_in_tg_back_end: {n_transactions_in_tg_back_end}")
-        print(f"IsUpdate: {IsUpdate}")
+        print(f"IsUpdate: {self.IsUpdate}, [{datetime.now()}]")
          
         print()
         print("-"*50)
@@ -101,61 +102,57 @@ class UpdateTransactions():
             ]
         }
         accounts_data = self.credit_info_of_tg_back_end.find(query)
-        self.dict_initial_credit = {i["accountId"]:i["creditAmount"] for i in accounts_data}
+        self.dict_initial_credit = {i["accountId"]:i["initialCredit"] for i in accounts_data}
         
-        print(f"Dict initial credit: {self.dict_initial_credit}")
+        # print(f"Dict initial credit: {self.dict_initial_credit}")
         
-        print()
-        print("-"*50)
-        print()
+        # print()
+        # print("-"*50)
+        # print()
         
-    def check_not_updated_id(self):
-        projection = {"_id": 1}
+    def check_not_updated_transaction_id(self):
+        projection = {"_id": -1}
         transactions_id_of_tg = set([i['_id'] for i in self.transactions_of_tg.find(projection=projection)])
         transactions_id_of_tg_back_end = set([i['_id'] for i in self.transactions_of_tg_back_end.find(projection=projection)])
-        list_not_updated_id = list(transactions_id_of_tg - transactions_id_of_tg_back_end)
+        self.list_not_updated_id = list(transactions_id_of_tg - transactions_id_of_tg_back_end)
         
-        for not_updated_id in list_not_updated_id[:1]:
-            not_updated_data = self.transactions_of_tg.find_one({"_id": not_updated_id})
-            accountId = not_updated_data['accountId']
-            initial_credit = self.dict_initial_credit[accountId]
-            print(f"not_updated_data: {not_updated_data}")
-            print(f"len(not_updated_data): {len(not_updated_data)}")
-            print(f"accountId: {accountId}")
-            print(f"initial_credit: {type(initial_credit)}")
+        print(f"List not updated id: {self.list_not_updated_id}")
             
         print()
         print("-"*50)
         print()
             
     ### Update credit amount to transactions database
-    # def update_credit_to_transactions(self):
-    #     self.dict_initial_credits = self.get_last_initial_credits()
-        
-    #     projection = {"_id": 1, "accountId": 1, "status": 1, "createdAt": 1}
-    #     transactions_data = self.transactions_of_tg_back_end.find(projection=projection)
-
-    #     try:
-    #         for transaction in transactions_data:
-    #             ### Add initial date
-    #             transaction_id = transaction['_id']
-    #             account_id = transaction['accountId']
-    #             credit_amount_by_account_id = self.dict_initial_credits[account_id]
+    def update_credit_to_transactions(self):
+        try:
+            for not_updated_id in self.list_not_updated_id:
+                not_updated_data = self.transactions_of_tg.find_one({"_id": not_updated_id})
                 
-    #             ### Update the credit
-    #             result = self.transactions_of_tg_back_end.update_one(
-    #                 {"_id": transaction_id, "accountId": account_id},
-    #                 {"$set": {"initialCredit": credit_amount_by_account_id}}
-    #             )
+                ### Insert the initial credit
+                accountId = not_updated_data['accountId']
+                initial_credit = self.dict_initial_credit[accountId]
+                not_updated_data['initialCredit'] = initial_credit
                 
-    #         print(f"[{datetime.now()}], Update credit amount to transactions in tg-back-end database completed")
-    #     except Exception as e:
-    #         print("[{datetime.now()}], Error to insert: {e}")
+                self.transactions_of_tg_back_end.insert_one(not_updated_data)
+                
+                print(f"_id: {not_updated_data['_id']}")
+                print(f"Insert the credit to the transactions completed, [{datetime.now()}]")
+                
+        except Exception as e:
+            print(f"Update the credit to the transactions error : {e}")
+            
+        print()
+        print("-"*50)
+        print()
         
     def run(self):
-        self.compare_n_transactions_between_tg_and_tg_back_end()
-        self.get_last_initial_credits()
-        self.check_not_updated_id()
+        while True:
+            self.compare_n_transactions_between_tg_and_tg_back_end()
+            if self.IsUpdate:
+                self.get_last_initial_credits()
+                self.check_not_updated_transaction_id()
+                self.update_credit_to_transactions()
+            time.sleep(1)
         
 if __name__ == "__main__":
     UpdateTransactions().run()

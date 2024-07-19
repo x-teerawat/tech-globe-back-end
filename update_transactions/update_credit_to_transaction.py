d0a42a299044db412ba24f37a85438407b6d5293
Teerawat Xu <xu.teerawat@gmail.com>
2024-07-20 00:18:42 +0700

    update insert transactions and update statuses

--- update_transactions/update_credit_to_transaction.py
+++ update_transactions/update_credit_to_transaction.py
@@ -1,13 +1,9 @@
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
-from datetime import datetime, time as dtime
 import schedule
-import pandas as pd
-import numpy as np
 import time
-from bson.objectid import ObjectId
+from pymongo import MongoClient
+from datetime import datetime
 
-class UpdateCreditToTransaction():
+class CheckDatabaseSimilarity():
     def __init__(self):
         ### สร้างการเชื่อมต่อกับ MongoDB
         client = MongoClient(f"mongodb://ubuntu:techglobetrading@13.229.230.27:27017")
@@ -17,41 +13,37 @@ class UpdateCreditToTransaction():
         tg_back_end_database = "tg-back-end"
         
         ### Collections
-        accounts_collection = "accounts"
         transactions_collection = "transactions"
-        credit_info_collection = "credit-info"
+        accounts_collection = "accounts"
+        credits_collection = "credits"
         
         ### tg database
         _tg_database = client[tg_database] # Select/Create database
-        self.accounts_of_tg = _tg_database[accounts_collection] # Select/Create collection
         self.transactions_of_tg = _tg_database[transactions_collection] # Select/Create collection
         
         ### tg-back-end database
         _tg_back_end_database = client[tg_back_end_database] # Select/Create database
-        self.credit_info_of_tg_back_end = _tg_back_end_database[credit_info_collection] # Select/Create collection
         self.transactions_of_tg_back_end = _tg_back_end_database[transactions_collection] # Select/Create collection
+        self.accounts_of_tg = _tg_database[accounts_collection] # Select/Create collection
+        self.credit_info_of_tg_back_end = _tg_back_end_database[credits_collection] # Select/Create collection
 
         try:
             # พยายามดึงข้อมูลเซิร์ฟเวอร์
             client.admin.command('ping')
             print("Connection successful.")
-        except ConnectionFailure:
-            print("Server not available.")   
-            
+        except Exception as e:
+            print(f"Server not available: {e}")   
+        
+        ### Get last initial credits
+        self.get_last_initial_credits()
         print()
         print("-"*50)
-        print() 
-            
+        print()
+        
     ### Get account id
     def get_account_id(self):
         projection = {"account_id": 1}
         self.list_account_ids = [i['_id'] for i in self.accounts_of_tg.find(projection=projection)]
-        
-        # print(f"All Account Id: {self.list_account_ids}")
-        
-        # print()
-        # print("-"*50)
-        # print()
         
     ### Get last date
     def get_last_date(self):
@@ -59,37 +51,10 @@ class UpdateCreditToTransaction():
         last_date_document = self.credit_info_of_tg_back_end.find_one(sort=[("date", -1)])
         if last_date_document:
             self.last_date = last_date_document.get("date")
-            
         else:
             print("No documents found in collection.")
             self.last_date =  None
-            
-        # print(f"Last date: {self.last_date}")  
         
-        # print()
-        # print("-"*50)
-        # print()
-        
-    def compare_n_transactions_between_tg_and_tg_back_end(self):
-        n_transactions_in_tg = self.transactions_of_tg.count_documents({})
-        n_transactions_in_tg_back_end = self.transactions_of_tg_back_end.count_documents({})
-        
-        if n_transactions_in_tg > n_transactions_in_tg_back_end:
-            self.IsUpdateTransaction = True
-        elif n_transactions_in_tg == n_transactions_in_tg_back_end:
-            self.IsUpdateTransaction = False
-        else:
-            print(f"n_transactions_in_tg ({n_transactions_in_tg}) < n_transactions_in_tg_back_end ({n_transactions_in_tg_back_end})")
-            self.IsUpdateTransaction = None
-        
-        print(f"n_transactions_in_tg: {n_transactions_in_tg}")
-        print(f"n_transactions_in_tg_back_end: {n_transactions_in_tg_back_end}")
-        print(f"IsUpdateTransaction: {self.IsUpdateTransaction}, [{datetime.now()}]")
-         
-        print()
-        print("-"*50)
-        print()
-
     ### Get last initial credits
     def get_last_initial_credits(self):
         self.get_account_id()
@@ -103,135 +68,70 @@ class UpdateCreditToTransaction():
         }
         accounts_data = self.credit_info_of_tg_back_end.find(query)
         self.dict_initial_credits = {i["accountId"]:i["initialCredit"] for i in accounts_data}
+
+    # สร้างฟังก์ชันเพื่อเปรียบเทียบข้อมูล
+    def compare_collections(self):
+        transactions_of_tg_data = list(self.transactions_of_tg.find({}))
+        transactions_of_tg_back_end_data = list(self.transactions_of_tg_back_end.find({}))
         
-        # print(f"Dict initial credit: {self.dict_initial_credits}")
+        # เปรียบเทียบเอกสารทีละเอกสาร
+        transactions_of_tg_data_ids = {doc['_id']: doc for doc in transactions_of_tg_data}
+        transactions_of_tg_back_end_data_ids = {doc['_id']: doc for doc in transactions_of_tg_back_end_data}
         
-        # print()
-        # print("-"*50)
-        # print()
+        all_transaction_ids = sorted(list(set(transactions_of_tg_data_ids.keys()).union(transactions_of_tg_back_end_data_ids.keys())))
         
-    def check_not_updated_transaction_id(self):
-        projection = {"_id": -1}
-        transactions_id_of_tg = set([i['_id'] for i in self.transactions_of_tg.find(projection=projection)])
-        transactions_id_of_tg_back_end = set([i['_id'] for i in self.transactions_of_tg_back_end.find(projection=projection)])
-        self.list_not_updated_transaction_id = list(transactions_id_of_tg - transactions_id_of_tg_back_end)
-        
-        print(f"List not updated id: {self.list_not_updated_transaction_id}")
-            
-        print()
-        print("-"*50)
-        print()
-            
-    ### Update credit amount to transactions database
-    def update_credit_to_transactions(self):
-        try:
-            for not_updated_transaction_id in self.list_not_updated_transaction_id:
-                not_updated_data = self.transactions_of_tg.find_one({"_id": not_updated_transaction_id})
+        # เปรียบเทียบจำนวนเอกสาร
+        if len(transactions_of_tg_data) != len(transactions_of_tg_back_end_data):
+            print("len(transactions_of_tg_data) != len(transactions_of_tg_back_end_data)")
+            for transaction_id in all_transaction_ids:
+                transactions_of_tg_doc = transactions_of_tg_data_ids.get(transaction_id)
+                transactions_of_tg_back_end_doc = transactions_of_tg_back_end_data_ids.get(transaction_id)
                 
-                ### Insert the initial credit
-                accountId = not_updated_data['accountId']
-                initial_credit = self.dict_initial_credits[accountId]
-                not_updated_data['initialCredit'] = initial_credit
-                
-                self.transactions_of_tg_back_end.insert_one(not_updated_data)
-                
-                print(f"_id: {not_updated_data['_id']}")
-                print(f"Insert the credit to the transactions completed, [{datetime.now()}]")
-                
-        except Exception as e:
-            print(f"Account id for the credit updating to the transactions error : {e}")
-            
-        print()
-        print("-"*50)
-        print()
-        
-    def compare_n_closed_status_between_tg_and_tg_back_end(self):
-        self.query_closed_status = {'status': 'closed'}
-        n_closed_status_in_tg = self.transactions_of_tg.count_documents(self.query_closed_status)
-        n_closed_status_in_tg_back_end = self.transactions_of_tg_back_end.count_documents(self.query_closed_status)
-        
-        if n_closed_status_in_tg > n_closed_status_in_tg_back_end:
-            self.IsUpdateStatus = True
-        elif n_closed_status_in_tg == n_closed_status_in_tg_back_end:
-            self.IsUpdateStatus = False
+                ### Similarity check
+                if transactions_of_tg_doc != transactions_of_tg_back_end_doc:
+                    if transactions_of_tg_back_end_doc is None:
+                        ### เพิ่มเอกสารใหม่
+                        transactions_of_tg_doc['initialCredit'] = self.dict_initial_credits[transactions_of_tg_doc['accountId']]
+                        self.transactions_of_tg_back_end.insert_one(transactions_of_tg_doc)
+                        print(f"Inserted document with transaction_id: {transaction_id} [{datetime.now()}]")
         else:
-            print(f"n_closed_status_in_tg ({n_closed_status_in_tg}) < n_closed_status_in_tg_back_end ({n_closed_status_in_tg_back_end})")
-            self.IsUpdateStatus = None
-        
-        print(f"n_closed_status_in_tg: {n_closed_status_in_tg}")
-        print(f"n_closed_status_in_tg_back_end: {n_closed_status_in_tg_back_end}")
-        print(f"IsUpdateStatus: {self.IsUpdateStatus}, [{datetime.now()}]")
-         
-        print()
-        print("-"*50)
-        print()
-        
-    def check_not_updated_status_id(self):
-        status_id_of_tg = set([i['_id'] for i in self.transactions_of_tg.find(self.query_closed_status)])
-        status_id_of_tg_back_end = set([i['_id'] for i in self.transactions_of_tg_back_end.find(self.query_closed_status)])
-        self.list_not_updated_status_id = sorted(list(status_id_of_tg - status_id_of_tg_back_end)) # Convert set to list type and ascending sort
-        
-        print(f"List not updated status id: {self.list_not_updated_status_id}")
-        print(f"Amount of not updated status id: {len(self.list_not_updated_status_id)}")
-            
-        print()
-        print("-"*50)
-        print()
-        
-    def update_status(self):
-        for not_updated_status_id in self.list_not_updated_status_id:
-            updated_status_data = self.transactions_of_tg.find_one({"_id": not_updated_status_id})
-            update_status_result = self.transactions_of_tg_back_end.update_one({'_id': not_updated_status_id}, {'$set': updated_status_data})
-            print(f'Update status at id: {not_updated_status_id}, modified count: {update_status_result.modified_count}')
-            
-        print()
-        print("-"*50)
-        print()
-        
-        
-    def run(self):
-        while True:
-            ### Check and update transactions
-            self.compare_n_transactions_between_tg_and_tg_back_end()
-            if self.IsUpdateTransaction:
-                self.get_last_initial_credits()
-                self.check_not_updated_transaction_id()
-                self.update_credit_to_transactions()
+            print(f"len(transactions_of_tg_data) == len(transactions_of_tg_back_end_data) [{datetime.now()}]")
+            for transaction_id in all_transaction_ids:
+                transactions_of_tg_doc = transactions_of_tg_data_ids.get(transaction_id)
+                transactions_of_tg_back_end_doc = transactions_of_tg_back_end_data_ids.get(transaction_id)
                 
-            ### Check and update statuses
-            self.compare_n_closed_status_between_tg_and_tg_back_end()
-            if self.IsUpdateStatus:
-                self.check_not_updated_status_id()
-                self.update_status()
-            
+                ### Status check
+                if transactions_of_tg_doc['status'] != transactions_of_tg_back_end_doc['status']:
+                    ### อัพเดตเอกสาร
+                    self.transactions_of_tg_back_end.update_one(
+                        {'_id': transaction_id},
+                        {'$set': transactions_of_tg_doc}
+                    )
+                    print(f"Updated document with transaction_id: {transaction_id} [{datetime.now()}]")
+
+    ### ฟังก์ชันที่จะรันเป็น schedule
+    def job(self):
+        while True:
+            now = datetime.now().time()
+            if now >= datetime.strptime("20:30", "%H:%M").time() or now <= datetime.strptime("05:00", "%H:%M").time():
+                self.compare_collections()
+            if now > datetime.strptime("05:00", "%H:%M").time():
+                print(f"Ending job at {now}")
+                break
             time.sleep(1)
-            
-            
-    ### Determine schedule
-    def is_within_time_range(self):
-        now = datetime.now()
-        current_time = now.time()
-        start_time_evening = dtime(20, 30)
-        end_time_morning = dtime(5, 0)
 
-        if now.weekday() < 5:  # Monday to Friday
-            if start_time_evening <= current_time or current_time <= end_time_morning:
-                return True
-        elif now.weekday() == 5:  # Saturday
-            if current_time <= end_time_morning:
-                return True
-        return False
-
-    def scheduled_job(self):
-        if self.is_within_time_range():
-            self.run()
+    ### ตั้งค่า schedule จันทร์-ศุกร์ 20:30 ถึง 05:00
+    def schedule_jobs(self):
+        schedule.every().monday.at("20:30").do(self.job)
+        schedule.every().tuesday.at("20:30").do(self.job)
+        schedule.every().wednesday.at("20:30").do(self.job)
+        schedule.every().thursday.at("20:30").do(self.job)
+        schedule.every().friday.at("20:30").do(self.job)
         
-if __name__ == "__main__":
-    # ### Schedule the job every second
-    # schedule.every(1).seconds.do(UpdateCreditToTransaction().scheduled_job)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
 
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-    
-    UpdateCreditToTransaction().run()
+if __name__ == "__main__":
+    # CheckDatabaseSimilarity().job()
+    CheckDatabaseSimilarity().schedule_jobs()
